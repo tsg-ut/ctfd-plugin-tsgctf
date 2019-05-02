06fd03bc844539d08b13c58e37d390bee6319b5c
Koki Takahashi <hakatasiloving@gmail.com>
2019-05-03 02:07:41 +0900

    Implement dates api endpoint

--- __init__.py
+++ __init__.py
@@ -1,8 +1,27 @@
-from flask import send_file
+from flask import send_file, jsonify
 from flask.helpers import safe_join
+from CTFd.utils import get_config
+from CTFd.utils.dates import ctf_ended, ctf_started
 
 def load(app):
 	@app.route("/OneSignalSDKWorker.js", methods=["GET"])
 	def worker():
 		filename = safe_join(app.root_path, 'themes', 'tsgctf', 'static', 'OneSignalSDKWorker.js')
 		return send_file(filename)
+
+	@app.route("/api/v1/dates", methods=["GET"])
+	def dates():
+		start = get_config('start')
+		end = get_config('end')
+		is_started = ctf_started()
+		is_ended = ctf_ended()
+
+		return jsonify({
+			'success': True,
+			'data': {
+				'start': start,
+				'end': end,
+				'is_started': is_started,
+				'is_ended': is_ended,
+			},
+		})
