122204741bd0d6427d265b561c1be09c97c78bf0
Koki Takahashi <hakatasiloving@gmail.com>
2019-05-03 03:18:48 +0900

    Add is_verified section to dates response

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import send_file, jsonify
 from flask.helpers import safe_join
 from CTFd.utils import get_config
 from CTFd.utils.dates import ctf_ended, ctf_started
+from CTFd.utils.user import is_verified, is_admin
 
 def load(app):
 	@app.route("/OneSignalSDKWorker.js", methods=["GET"])
@@ -23,5 +24,6 @@ def load(app):
 				'end': end,
 				'is_started': is_started,
 				'is_ended': is_ended,
+				'is_verified': is_verified() or is_admin(),
 			},
 		})
